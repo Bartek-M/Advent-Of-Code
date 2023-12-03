e1b0fe5ce850a823c30ff9a1ea6566a4085f401b
Bartek <bmroczkowski@icloud.com>
2023-12-03 23:17:35 +0100

    Day 3 | 2023

---
+++ 2023/day_3.py
@@ -0,0 +1,137 @@
+# Advent of code day 3 2023
+# https://adventofcode.com/2023/day/3
+print("Advent of code day 3 2023\n")
+
+with open("./input/day_3.in", "r") as f:
+    data = [list(line.strip()) for line in f.readlines()]
+
+
+# Part 1
+class Node:
+    def __init__(self, grid, row, col, length):
+        self.grid = grid
+
+        self.row = row
+        self.col = col
+        self.length = length
+
+        self.max_width = len(grid[row])
+        self.max_height = len(grid)
+
+        self.value = int("".join(grid[row][col : col + length]))
+        self.neighbors = self.get_neighbors()
+
+    def get_neighbors(self):
+        x, y = self.col, self.row
+
+        for dx in range(-1, self.length + 1):
+            if x + dx < 0 or x + dx >= self.max_width:
+                continue
+
+            for dy in range(-1, 2):
+                if y + dy < 0 or y + dy >= self.max_height:
+                    continue
+
+                if dy == 0 and dx in range(self.length):
+                    continue
+
+                char = self.grid[y + dy][x + dx]
+
+                if not char.isdigit() and char != ".":
+                    return True
+
+        return None
+
+
+sum_nodes = 0
+
+for i in range(len(data)):
+    current = ""
+    row = data[i] + ["."]
+
+    for ind, char in enumerate(row):
+        if char.isdigit():
+            current += char
+            continue
+
+        if not current:
+            continue
+
+        node = Node(data, i, ind - len(current), len(current))
+        sum_nodes += node.value if node.neighbors else 0
+
+        current = ""
+
+print("[PART 1]", sum_nodes)
+
+
+# Part 2
+class Node:
+    def __init__(self, grid, row, col, length):
+        self.grid = grid
+
+        self.row = row
+        self.col = col
+        self.length = length
+
+        self.max_width = len(grid[row])
+        self.max_height = len(grid)
+
+        self.value = int("".join(grid[row][col : col + length]))
+        self.neighbors = self.get_neighbors()
+
+    def get_neighbors(self):
+        x, y = self.col, self.row
+
+        for dx in range(-1, self.length + 1):
+            if x + dx < 0 or x + dx >= self.max_width:
+                continue
+
+            for dy in range(-1, 2):
+                if y + dy < 0 or y + dy >= self.max_height:
+                    continue
+
+                if dy == 0 and dx in range(self.length):
+                    continue
+
+                if self.grid[y + dy][x + dx] == "*":
+                    return (y + dy, x + dx)
+
+        return None
+
+
+nodes = {}
+sum_nodes = 0
+
+for i in range(len(data)):
+    current = ""
+    row = data[i] + ["."]
+
+    for ind, char in enumerate(row):
+        if char.isdigit():
+            current += char
+            continue
+
+        if not current:
+            continue
+
+        node = Node(data, i, ind - len(current), len(current))
+        neighbor = node.neighbors
+
+        if neighbor:
+            if neighbor in nodes:
+                nodes[neighbor].append(node)
+            else:
+                nodes[neighbor] = [node]
+
+        current = ""
+
+for key in nodes:
+    node_list = nodes[key]
+
+    if len(node_list) != 2:
+        continue
+
+    sum_nodes += node_list[0].value * node_list[1].value
+
+print("[PART 2]", sum_nodes)
